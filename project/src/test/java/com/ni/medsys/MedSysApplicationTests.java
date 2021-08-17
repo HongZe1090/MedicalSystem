6717c9b52b00dc73f6044b2b244262a3246f99ab
1090414202 <1090414202@qq.com>
2021-08-18 01:11:01 +0800

    项目重构，使用springboot

---
+++ project/src/test/java/com/ni/medsys/MedSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ni.medsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
